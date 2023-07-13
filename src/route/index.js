15f4a690a855927a0923e61f1b1fc2115032e2d3
GenaroFlores <davidenko_rad@ukr.net>
2023-07-13 14:49:05 +0300

    init my first template

--- src/route/index.js
+++ src/route/index.js
@@ -26,8 +26,9 @@ router.get('/template-1', function (req, res) {
 
   //            ↙ cюди вводимо назву файлу з сontainer
   res.render('template-1', {
+    layout: 'template-1',
     page: {
-      title: 'Example Page',
+      title: 'Template 1 Page',
     },
     header: [
       {
